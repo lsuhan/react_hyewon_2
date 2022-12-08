dcc1ef706747a369144665a7a34da31f5f648a7e
lsuhan00-at-391858724554 <lsuhan00>
2022-12-08 18:32:04 +0900

    commit

---
+++ src/main/java/com/example/reacts/ReactsApplication.java
@@ -0,0 +1,13 @@
+package com.example.reacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/reacts/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.reacts;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ReactsApplication.class);
+    }
+
+}
